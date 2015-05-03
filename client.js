e9cdbf72e30ebfda0e688c24d3c494f2b3314492
Rhys Evans <wheresrhys@gmail.com>
2015-05-03 19:10:58 +0100

    mockResponse is DRYer

--- client.js
+++ client.js
@@ -1,36 +1,10 @@
 'use strict';
 
-var Response = window.Response;
-var Headers = window.Headers;
-
-function mockResponse (url, config) {
-	// allow just body to be passed in as this is the commonest use case
-	if (typeof config === 'string' || !(config.body || config.headers || config.throws || config.status)) {
-		config = {
-			body: config
-		};
-	}
-	if (config.throws) {
-		return Promise.reject(config.throws);
-	}
-	var opts = config.opts || {};
-	opts.url = url;
-	opts.status = config.status || 200;
-	opts.headers = config.headers ? new Headers(config.headers) : new Headers();
-
-	if (config.body != null) {
-		var body = config.body;
-		if (typeof body === 'object') {
-			body = JSON.stringify(body);
-		}
-	}
-
-	return Promise.resolve(new Response(body, opts));
-}
-
 var FetchMock = require('./src/fetch-mock');
 
 module.exports = new FetchMock({
-	mockResponse: mockResponse,
-	theGlobal: window
+	theGlobal: window,
+	Response: window.Response,
+	Headers: window.Headers,
+	Blob: window.Blob
 });
--- server.js
+++ server.js
@@ -3,38 +3,11 @@
 var Response = require('node-fetch').Response;
 var Headers = require('node-fetch').Headers;
 var stream = require('stream');
-
-function mockResponse (url, config) {
-	// allow just body to be passed in as this is the commonest use case
-	if (typeof config === 'string' || !(config.body || config.headers || config.throws || config.status)) {
-		config = {
-			body: config
-		};
-	}
-	if (config.throws) {
-		return Promise.reject(config.throws);
-	}
-	var opts = config.opts || {};
-	opts.url = url;
-	opts.status = config.status || 200;
-	opts.headers = config.headers ? new Headers(config.headers) : new Headers();
-
-	var s = new stream.Readable();
-	if (config.body != null) {
-		var body = config.body;
-		if (typeof body === 'object') {
-			body = JSON.stringify(body);
-		}
-		s.push(body, 'utf-8');
-	}
-
-	s.push(null);
-	return Promise.resolve(new Response(s, opts));
-}
-
 var FetchMock = require('./src/fetch-mock');
 
 module.exports = new FetchMock({
-	mockResponse: mockResponse,
-	theGlobal: GLOBAL
+	theGlobal: GLOBAL,
+	Response: Response,
+	Headers: Headers,
+	stream: stream
 });

eebc54809826fb1f607ea849cd36f83160f6973a
Rhys Evans <rhys.evans@ft.com>
2015-11-09 14:54:36 +0000

    corrected error message

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 'use strict';
 
 if (typeof window === 'undefined') {
-	throw 'Incorrect fetch-mock: require(\'fetch-mock/server\') in nodejs >= 4, or require(\'fetch-mock/es5/server\' in nodejs 0.12.x';
+	throw 'Incorrect fetch-mock: require(\'fetch-mock/src/server\') in nodejs >= 4, or require(\'fetch-mock/es5/server\' in nodejs 0.12.x';
 }
 
 const FetchMock = require('./fetch-mock');

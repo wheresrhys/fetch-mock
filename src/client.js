d13d520183b3e3c0dc414115732c7a202b6251bf
timnew <timnew.wti@gmail.com>
2016-03-17 11:23:28 +0800

    Add statusText to mocked response

--- src/client.js
+++ src/client.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const FetchMock = require('./fetch-mock');
+const STATUS_TEXT = require('./statusText');
 
 module.exports = new FetchMock({
 	theGlobal: window,
 	Request: window.Request,
 	Response: window.Response,
 	Headers: window.Headers,
+	STATUS_TEXT: STATUS_TEXT,
 	debug: function () {}
 });

e2a1120a649fd3092378c1b7f4e12d96dfb3c99a
Rhys Evans <wheresrhys@gmail.com>
2017-12-14 21:39:25 +0000

    a little file splitting

---
+++ src/lib/inspecting.js
@@ -0,0 +1,64 @@
+const FetchMock = {};
+
+FetchMock.calls = function (name) {
+	return name ? (this._calls[name] || []) : {
+		matched: this._matchedCalls,
+		unmatched: this._unmatchedCalls
+	};
+}
+
+FetchMock.lastCall = function (name) {
+	const calls = name ? this.calls(name) : this.calls().matched;
+	if (calls && calls.length) {
+		return calls[calls.length - 1];
+	} else {
+		return undefined;
+	}
+}
+
+FetchMock.lastUrl = function (name) {
+	const call = this.lastCall(name);
+	return call && call[0];
+}
+
+FetchMock.lastOptions = function (name) {
+	const call = this.lastCall(name);
+	return call && call[1];
+}
+
+FetchMock.called = function (name) {
+	if (!name) {
+		return !!(this._matchedCalls.length || this._unmatchedCalls.length);
+	}
+	return !!(this._calls[name] && this._calls[name].length);
+}
+
+FetchMock.done = function (name) {
+	const names = name ? [name] : this.routes.map(r => r.name);
+	// Can't use array.every because
+	// a) not widely supported
+	// b) would exit after first failure, which would break the logging
+	return names.map(name => {
+		if (!this.called(name)) {
+			console.warn(`Warning: ${name} not called`);
+			return false;
+		}
+		// would use array.find... but again not so widely supported
+		const expectedTimes = (this.routes.filter(r => r.name === name) || [{}])[0].repeat;
+
+		if (!expectedTimes) {
+			return true;
+		}
+
+		const actualTimes = this.calls(name).length;
+		if (expectedTimes > actualTimes) {
+			console.warn(`Warning: ${name} only called ${actualTimes} times, but ${expectedTimes} expected`);
+			return false;
+		} else {
+			return true;
+		}
+	})
+		.filter(bool => !bool).length === 0
+};
+
+module.exports = FetchMock;

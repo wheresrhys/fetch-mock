efeac761c92a740240677a40e82e9aa39c72b8b4
Rhys Evans <wheresrhys@gmail.com>
2018-01-21 15:58:53 +0000

    transform-runtime

--- karma.conf.js
+++ karma.conf.js
@@ -6,7 +6,6 @@ module.exports = function(karma) {
 
 		frameworks: [ 'mocha', 'chai', 'browserify'],
 		files: [
-			'http://cdn.polyfill.io/v2/polyfill.min.js?features=default,Promise,fetch,Array.prototype.map,Array.prototype.filter',
 			'test/client.js',
 			'test/sw.js',
 			{pattern: 'test/fixtures/built-sw.js', served: true, included: false},
@@ -19,6 +18,7 @@ module.exports = function(karma) {
 		},
 		browserify: {
 			debug: true,
+			plugins: ['transform-runtime'],
 			transform: [
 				['babelify', {
 					'presets': ['env']

25dc185069463a74467775841218171c1b5635bb
Stephan Rodemeier <stephan.rodemeier@mailbox.org>
2019-02-14 15:16:10 +0100

    Respect the loglevel for postinstall
    This adds support for disabling the postinstall message on CI and when
    the loglevel asks for it.

--- scripts/support-fetch-mock.js
+++ scripts/support-fetch-mock.js
@@ -1,17 +1,26 @@
 /* eslint-disable no-console */
+function isTrue(value) {
+	return !!value && value !== "0" && value !== "false"
+}
 
-const green = '\u001b[32m';
-const white = '\u001b[22m\u001b[39m';
-const boldCyan = '\u001b[96m\u001b[1m';
-const reset = '\u001b[0m';
+var envDisable = isTrue(process.env.CI);
+var logLevel = process.env.npm_config_loglevel;
+var logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1;
 
-const output =
-	green +
-	'Have some ❤️ for fetch-mock? Why not donate to my charity of choice:' +
-	white +
-	'\n > ' +
-	boldCyan +
-	'https://www.justgiving.com/refugee-support-europe\n' +
-	reset;
+if (!envDisable && !logLevelDisplay) {
+	const green = '\u001b[32m';
+	const white = '\u001b[22m\u001b[39m';
+	const boldCyan = '\u001b[96m\u001b[1m';
+	const reset = '\u001b[0m';
 
-console.log(output);
+	const output =
+		green +
+		'Have some ❤️ for fetch-mock? Why not donate to my charity of choice:' +
+		white +
+		'\n > ' +
+		boldCyan +
+		'https://www.justgiving.com/refugee-support-europe\n' +
+		reset;
+
+	console.log(output);
+}

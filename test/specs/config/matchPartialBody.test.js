12008da7aa1e9c7e4dc1b1c9c0a8951c3abf5960
Rhys Evans <rhys.evans@ft.com>
2020-05-18 00:02:44 +0100

    moisten

--- test/specs/config/matchPartialBody.test.js
+++ test/specs/config/matchPartialBody.test.js
@@ -9,37 +9,34 @@ describe('matchPartialBody', () => {
 		fm = fetchMock.createInstance();
 	});
 
-	const mock = (options) =>
-		fm.mock({ body: { ham: 'sandwich' }, ...options }, 200).catch(404);
-
 	const postExpect = async (expectedStatus) => {
 		const { status } = await fm.fetchHandler('http://a.com', {
 			method: 'POST',
-			body: JSON.stringify({ ham: 'sandwich', egg: 'mayonaise' }),
+			body: JSON.stringify({ a: 1, b: 2 }),
 		});
 		expect(status).to.equal(expectedStatus);
 	};
 
 	it("don't match partial bodies by default", async () => {
-		mock();
+		fm.mock({ body: { a: 1 } }, 200).catch(404);
 		await postExpect(404);
 	});
 
 	it('match partial bodies when configured true', async () => {
 		fm.config.matchPartialBody = true;
-		mock();
+		fm.mock({ body: { a: 1 } }, 200).catch(404);
 		await postExpect(200);
 	});
 
 	it('local setting can override to false', async () => {
 		fm.config.matchPartialBody = true;
-		mock({ matchPartialBody: false });
+		fm.mock({ body: { a: 1 }, matchPartialBody: false }, 200).catch(404);
 		await postExpect(404);
 	});
 
 	it('local setting can override to true', async () => {
 		fm.config.matchPartialBody = false;
-		mock({ matchPartialBody: true });
+		fm.mock({ body: { a: 1 }, matchPartialBody: true }, 200).catch(404);
 		await postExpect(200);
 	});
 });

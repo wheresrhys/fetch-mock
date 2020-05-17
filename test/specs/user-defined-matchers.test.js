10fe11d461e3eecfc46b45aa893a0fe6de794015
Rhys Evans <rhys.evans@ft.com>
2020-05-17 23:56:56 +0100

    clean up user defined matchers test

--- test/specs/user-defined-matchers.test.js
+++ test/specs/user-defined-matchers.test.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 
 const { fetchMock } = testGlobals;
 describe('user defined matchers', () => {
@@ -7,47 +9,74 @@ describe('user defined matchers', () => {
 		const fm = fetchMock.createInstance();
 		fm.addMatcher({
 			name: 'syncMatcher',
-			matcher: (route) => (url) => url.indexOf(route.syncMatcher) > 10,
+			matcher: (route) => (url) => url.indexOf(route.syncMatcher) > -1,
 		});
 		fm.mock(
 			{
-				syncMatcher: 'lime',
+				syncMatcher: 'a',
 			},
 			200
 		).catch();
-		await fm.fetchHandler('http://lime.at.there');
+		await fm.fetchHandler('http://b.com');
 		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.there/lime');
+		await fm.fetchHandler('http://a.com');
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
 	it('match on async body property', async () => {
 		const fm = fetchMock.createInstance();
 		fm.addMatcher({
-			name: 'asyncBodyMatcher',
-			matcher: (route) => (url, options, request) =>
-				request && JSON.parse(options.body)[route.asyncBodyMatcher] === true,
+			name: 'bodyMatcher',
+			matcher: (route) => (url, options) =>
+				JSON.parse(options.body)[route.bodyMatcher] === true,
 			usesBody: true,
 		});
 		fm.mock(
 			{
-				asyncBodyMatcher: 'lime',
+				bodyMatcher: 'a',
 			},
 			200
 		).catch();
 		await fm.fetchHandler(
-			new fm.config.Request('http://it.at.there', {
+			new fm.config.Request('http://a.com', {
 				method: 'POST',
-				body: JSON.stringify({ lemon: true }),
+				body: JSON.stringify({ b: true }),
 			})
 		);
 		expect(fm.calls(true).length).to.equal(0);
 		await fm.fetchHandler(
-			new fm.config.Request('http://it.at.there', {
+			new fm.config.Request('http://a.com', {
 				method: 'POST',
-				body: JSON.stringify({ lime: true }),
+				body: JSON.stringify({ a: true }),
 			})
 		);
-		expect(fm.calls(true).length).to.equal(1);
+		await fm.fetchHandler('http://a.com', {
+			method: 'POST',
+			body: JSON.stringify({ a: true }),
+		});
+		expect(fm.calls(true).length).to.equal(2);
+	});
+
+	it('not match on async body property without passing `usesBody: true`', async () => {
+		const fm = fetchMock.createInstance();
+		fm.addMatcher({
+			name: 'bodyMatcher',
+			matcher: (route) => (url, options) =>
+				JSON.parse(options.body)[route.bodyMatcher] === true,
+		});
+		fm.mock(
+			{
+				bodyMatcher: 'a',
+			},
+			200
+		).catch();
+		expect(() =>
+			fm.fetchHandler(
+				new fm.config.Request('http://a.com', {
+					method: 'POST',
+					body: JSON.stringify({ a: true }),
+				})
+			)
+		).to.throw();
 	});
 });

e5c28766d84672b9e4973f8a92e32aabb90f9776
Rhys Evans <rhys.evans@ft.com>
2020-05-19 21:44:23 +0100

    matcher-object test rewrite

--- test/specs/routing/matcher-object.test.js
+++ test/specs/routing/matcher-object.test.js
@@ -9,27 +9,21 @@ describe('matcher object', () => {
 	});
 
 	it('use matcher object with matcher property', async () => {
-		fm.mock({ matcher: 'http://it.at.there/path' }, 200).catch();
-		await fm.fetchHandler('http://it.at.there/path');
-		expect(fm.calls(true).length).to.equal(1);
-		await fm.fetchHandler('http://it.at.there/path/abouts');
-		await fm.fetchHandler('http://it.at.the');
+		fm.mock({ matcher: 'http://a.com' }, 200).catch();
+		await fm.fetchHandler('http://a.com');
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
 	it('use matcher object with url property', async () => {
-		fm.mock({ url: 'http://it.at.there/path' }, 200).catch();
-		await fm.fetchHandler('http://it.at.there/path');
-		expect(fm.calls(true).length).to.equal(1);
-		await fm.fetchHandler('http://it.at.there/path/abouts');
-		await fm.fetchHandler('http://it.at.the');
+		fm.mock({ url: 'http://a.com' }, 200).catch();
+		await fm.fetchHandler('http://a.com');
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
 	it('can use matcher and url simultaneously', async () => {
 		fm.mock(
 			{
-				url: 'end:profile',
+				url: 'end:path',
 				matcher: (url, opts) => {
 					return opts && opts.headers && opts.headers.authorized === true;
 				},
@@ -37,15 +31,22 @@ describe('matcher object', () => {
 			200
 		).catch();
 
-		await fm.fetchHandler('http://it.at.there/profile');
+		await fm.fetchHandler('http://a.com/path');
 		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.there/not', {
+		await fm.fetchHandler('http://a.com', {
 			headers: { authorized: true },
 		});
 		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.there/profile', {
+		await fm.fetchHandler('http://a.com/path', {
 			headers: { authorized: true },
 		});
+		expect(fm.calls(true).length).to.equal(1);
+	});
+
+	it('if no url provided, match any url', async () => {
+		fm.mock({}, 200).catch();
+
+		await fm.fetchHandler('http://a.com');
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
@@ -62,32 +63,29 @@ describe('matcher object', () => {
 	});
 
 	it('can match Headers', async () => {
-		fm.mock({ url: 'http://it.at.there/', headers: { a: 'b' } }, 200).catch();
+		fm.mock({ url: 'http://a.com', headers: { a: 'b' } }, 200).catch();
 
-		await fm.fetchHandler('http://it.at.there/', {
+		await fm.fetchHandler('http://a.com', {
 			headers: { a: 'c' },
 		});
 		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.there/', {
+		await fm.fetchHandler('http://a.com', {
 			headers: { a: 'b' },
 		});
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
 	it('can match query string', async () => {
-		fm.mock({ url: 'http://it.at.there/', query: { a: 'b' } }, 200).catch();
+		fm.mock({ url: 'http://a.com', query: { a: 'b' } }, 200).catch();
 
-		await fm.fetchHandler('http://it.at.there');
+		await fm.fetchHandler('http://a.com');
 		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.there?a=b');
+		await fm.fetchHandler('http://a.com?a=b');
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
 	it('can match path parameter', async () => {
-		fm.mock(
-			{ url: 'express:/type/:instance', params: { instance: 'b' } },
-			200
-		).catch();
+		fm.mock({ url: 'express:/type/:var', params: { var: 'b' } }, 200).catch();
 		await fm.fetchHandler('/');
 		expect(fm.calls(true).length).to.equal(0);
 		await fm.fetchHandler('/type/a');
@@ -97,23 +95,23 @@ describe('matcher object', () => {
 	});
 
 	it('can match method', async () => {
-		fm.mock({ url: 'http://it.at.there/', method: 'POST' }, 200).catch();
+		fm.mock({ method: 'POST' }, 200).catch();
 
-		await fm.fetchHandler('http://it.at.there/', { method: 'GET' });
+		await fm.fetchHandler('http://a.com', { method: 'GET' });
 		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.there/', { method: 'POST' });
+		await fm.fetchHandler('http://a.com', { method: 'POST' });
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
 	it('can match body', async () => {
-		fm.mock({ url: 'http://it.at.there/', body: { foo: 'bar' } }, 200).catch();
+		fm.mock({ body: { foo: 'bar' } }, 200).catch();
 
-		await fm.fetchHandler('http://it.at.there/', {
+		await fm.fetchHandler('http://a.com', {
 			method: 'POST',
 		});
 		expect(fm.calls(true).length).to.equal(0);
 
-		await fm.fetchHandler('http://it.at.there/', {
+		await fm.fetchHandler('http://a.com', {
 			method: 'POST',
 			body: JSON.stringify({ foo: 'bar' }),
 			headers: { 'Content-Type': 'application/json' },
@@ -121,42 +119,22 @@ describe('matcher object', () => {
 		expect(fm.calls(true).length).to.equal(1);
 	});
 
-	it('if no url provided, match any url', async () => {
-		fm.mock(
-			{
-				headers: { a: 'b' },
-			},
-			200
-		).catch();
-
-		await fm.fetchHandler('http://it.at.anywhere/', {
-			headers: { a: 'c' },
-		});
-		expect(fm.calls(true).length).to.equal(0);
-		await fm.fetchHandler('http://it.at.anywhere/', {
-			headers: { a: 'b' },
-		});
-		expect(fm.calls(true).length).to.equal(1);
-	});
-
 	it('support setting overwrite routes on matcher parameter', async () => {
 		expect(() =>
 			fm
-				.mock('http://it.at.there/', 200)
-				.mock({ url: 'http://it.at.there/', overwriteRoutes: true }, 300)
+				.mock('http://a.com', 200)
+				.mock({ url: 'http://a.com', overwriteRoutes: true }, 300)
 		).not.to.throw();
 
-		const res = await fm.fetchHandler('http://it.at.there/');
+		const res = await fm.fetchHandler('http://a.com');
 		expect(res.status).to.equal(300);
 	});
 
 	it('support setting matchPartialBody on matcher parameter', async () => {
-		fm.mock({ body: { ham: 'sandwich' }, matchPartialBody: true }, 200).catch(
-			404
-		);
-		const res = await fm.fetchHandler('http://it.at.there', {
+		fm.mock({ body: { a: 1 }, matchPartialBody: true }, 200).catch(404);
+		const res = await fm.fetchHandler('http://a.com', {
 			method: 'POST',
-			body: JSON.stringify({ ham: 'sandwich', egg: 'mayonaise' }),
+			body: JSON.stringify({ a: 1, b: 2 }),
 		});
 		expect(res.status).to.equal(200);
 	});

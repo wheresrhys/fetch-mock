0e38fb57fb9b79055fed6a5ae51970bbc1a44319
Rhys Evans <rhys.evans@ft.com>
2020-05-31 17:31:30 +0100

    use custom option compatible with node-fetch 2 or 3

--- test/specs/routing/function-matching.test.js
+++ test/specs/routing/function-matching.test.js
@@ -62,8 +62,8 @@ describe('function matching', () => {
 		// same properties in the browser and nodejs
 		const propertyToCheck = new fm.config.Request('http://example.com').cache
 			? 'credentials'
-			: 'highWaterMark';
-		const valueToSet = propertyToCheck === 'credentials' ? 'include' : 5000;
+			: 'compress';
+		const valueToSet = propertyToCheck === 'credentials' ? 'include' : false;
 
 		fm.mock(
 			(url, options, request) => request[propertyToCheck] === valueToSet,

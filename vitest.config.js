3161ff04f7cc68170dc8c7d7e3ce9e76bab34cbc
Rhys Evans <rhys.evans@ft.com>
2023-12-21 20:55:27 +0000

    attempt to get vitest working in the browser again

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,24 @@
 import { defineConfig } from 'vitest/config'
 
 const configs = {
-browser: {
-  setupFiles: './test/setup/browser.js' ,
-  browser: {
-      enabled: true,
-      name: 'chrome', // browser name is required
-    }
-},
+	browser: {
+	  setupFiles: './test/setup/browser.js' ,
+	  browser: {
+	      enabled: true,
+	      name: 'chrome', // browser name is required
+	  },
+	  server: {
+	    deps: {
+	      inline: [
+	        "brace-expansion"
+	      ]
+	    }
+	  }
+	},
 
-server: {
-setupFiles: './test/setup/server.js'
-}
+	server: {
+		setupFiles: './test/setup/server.js'
+	}
 }
 
 export default defineConfig({

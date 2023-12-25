4361e20935d722e3d0c2b795224aee3687b9cf7a
Rhys Evans <rhys.evans@ft.com>
2023-12-25 17:29:57 +0000

    assume native abortcontroller

--- test/setup/server.js
+++ test/setup/server.js
@@ -1,10 +1,8 @@
 import fetch from 'node-fetch';
-import AbortController from 'abort-controller';
 import fetchMock from '../../src/server.js';
 
 globalThis.testGlobals = {
 	fetchMock,
 	theGlobal: global,
 	fetch,
-	AbortController,
 };

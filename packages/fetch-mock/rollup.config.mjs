0dfd6c318c60c1ba33bb3ebefdabf32c082c051b
Rhys Evans <wheresrhys@gmail.com>
2024-07-24 15:16:41 +0100

    chore: share rollup config

--- packages/fetch-mock/rollup.config.mjs
+++ packages/fetch-mock/rollup.config.mjs
@@ -1,18 +1,2 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { writeFile, mkdir } from 'fs/promises';
-export default {
-	input: './src/index.js',
-	output: {
-		dir: './dist',
-		entryFileNames: 'commonjs.js',
-		format: 'commonjs',
-	},
-	plugins: [
-		nodeResolve({ preferBuiltins: false }),
-		commonjs(),
-		// sourcemaps(),
-		// builtins(),
-		// globals(),
-	],
-};
+import rollupConfig from '../../shared-rollup.config.js';
+export default rollupConfig;

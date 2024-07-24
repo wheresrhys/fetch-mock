87a2288173acdc829f58aaada380e88d5887ab61
Rhys Evans <wheresrhys@gmail.com>
2024-07-24 17:25:54 +0100

    chore: lint rollup config

--- shared-rollup.config.js
+++ shared-rollup.config.js
@@ -1,18 +1,17 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { writeFile, mkdir } from 'fs/promises';
 export default {
-    input: './src/index.js',
-    output: {
-        dir: './dist',
-        entryFileNames: 'commonjs.js',
-        format: 'commonjs',
-    },
-    plugins: [
-        nodeResolve({ preferBuiltins: false }),
-        commonjs(),
-        // sourcemaps(),
-        // builtins(),
-        // globals(),
-    ],
+	input: './src/index.js',
+	output: {
+		dir: './dist',
+		entryFileNames: 'commonjs.js',
+		format: 'commonjs',
+	},
+	plugins: [
+		nodeResolve({ preferBuiltins: false }),
+		commonjs(),
+		// sourcemaps(),
+		// builtins(),
+		// globals(),
+	],
 };

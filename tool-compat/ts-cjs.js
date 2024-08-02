169447d79ddd7cef18b61e9529e21abd212145dd
Rhys Evans <wheresrhys@gmail.com>
2024-08-02 17:17:16 +0100

    build: added new tests to CI

--- tool-compat/ts-cjs.js
+++
@@ -1,5 +0,0 @@
-Object.defineProperty(exports, '__esModule', { value: true });
-const fetchMockCore = require('@fetch-mock/core').default;
-fetchMockCore.route('http://example.com', 200);
-const fetchMock = require('fetch-mock').default;
-fetchMock.mock('http://example.com', 200);

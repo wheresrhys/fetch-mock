2bff326063a362ea4ce0adc1102130bd1c31ac9e
Rhys Evans <wheresrhys@gmail.com>
2024-08-15 14:42:24 +0100

    feat: export more types from @fetch-mock/core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,24 @@
-import fetchMock, { FetchMock } from './FetchMock.js';
+import fetchMock, {
+	FetchMockGlobalConfig,
+	FetchImplementations,
+	FetchMockConfig,
+	FetchMock,
+} from './FetchMock.js';
+import { CallHistoryFilter, CallLog } from './CallHistory.js';
+import { RouteMatcher, MatcherDefinition } from './Matchers.js';
+import { UserRouteConfig, RouteResponse, RouteName } from './Route.js';
+
+export {
+	FetchMockGlobalConfig,
+	FetchImplementations,
+	FetchMockConfig,
+	FetchMock,
+	CallHistoryFilter,
+	CallLog,
+	RouteMatcher,
+	MatcherDefinition,
+	UserRouteConfig,
+	RouteResponse,
+	RouteName,
+};
 export default fetchMock;
-export { FetchMock };

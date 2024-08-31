5577416234798e0c205258c55b1a431f6b6666a8
Rhys Evans <wheresrhys@gmail.com>
2024-08-31 17:34:57 +0100

    test: test suite for identifying fetch-mock instances

---
+++ packages/codemods/src/__test__/identifying-fetchmock-instances.test.js
@@ -0,0 +1,58 @@
+import { describe, it, expect } from 'vitest';
+import { codemod } from '../index';
+import jscodeshift from 'jscodeshift';
+
+function expectCodemodResult(src, expected) {
+	expect(codemod(src, jscodeshift)).toEqual(expected);
+}
+
+describe('identifying fetch-mock instances', () => {
+	it('cjs require() named fetchMock', () => {
+		expectCodemodResult(
+			`
+            const fetchMock = require('fetch-mock');
+            fetchMock.mock("blah", 200)
+        `,
+			`
+            const fetchMock = require('fetch-mock');
+            fetchMock.route("blah", 200)
+        `,
+		);
+	});
+	it('cjs require() named something else', () => {
+		expectCodemodResult(
+			`
+            const fetchNot = require('fetch-mock');
+            fetchNot.mock("blah", 200)
+        `,
+			`
+            const fetchNot = require('fetch-mock');
+            fetchNot.route("blah", 200)
+        `,
+		);
+	});
+	it('esm import named fetchMock', () => {
+		expectCodemodResult(
+			`
+            import fetchMock from 'fetch-mock';
+            fetchMock.mock("blah", 200)
+        `,
+			`
+            import fetchMock from 'fetch-mock';
+            fetchMock.route("blah", 200)
+        `,
+		);
+	});
+	it('esm import named something else', () => {
+		expectCodemodResult(
+			`
+            import fetchMock as fetchNot from 'fetch-mock';
+            fetchNot.mock("blah", 200)
+        `,
+			`
+            import fetchMock as fetchNot from 'fetch-mock';
+            fetchNot.route("blah", 200)
+        `,
+		);
+	});
+});

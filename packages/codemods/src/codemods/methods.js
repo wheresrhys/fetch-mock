a96f62b60e95e691dd0f783de419db01a1b92302
Rhys Evans <wheresrhys@gmail.com>
2024-09-15 18:06:57 +0100

    feat: codemod reset()

--- packages/codemods/src/codemods/methods.js
+++ packages/codemods/src/codemods/methods.js
@@ -55,6 +55,36 @@ export function simpleMethods(fetchMockVariableName, root, j) {
 		});
 	});
 
+	root
+		.find(j.CallExpression, {
+			callee: {
+				object: {
+					type: 'Identifier',
+					name: fetchMockVariableName,
+				},
+				property: {
+					name: 'reset',
+				},
+			},
+		})
+		.forEach((path) => {
+			const newExpressions = j(`
+fetchMock.clearHistory();
+fetchMock.removeRoutes({
+	includeFallback: true,
+});
+fetchMock.unmockGlobal();
+`)
+				.find(j.ExpressionStatement)
+				.paths();
+			const insertLocation = j(path)
+				.closest(j.ExpressionStatement)
+				.replaceWith(newExpressions.shift().value);
+			while (newExpressions.length) {
+				insertLocation.insertAfter(newExpressions.pop().value);
+			}
+		});
+
 	[
 		['lastUrl', 'url'],
 		['lastOptions', 'options'],

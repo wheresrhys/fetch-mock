308df960c1932eac3cf205f6e2e8866220267d3b
Rhys Evans <wheresrhys@gmail.com>
2024-11-13 21:02:11 +0000

    docs: wip add search

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
 			}),
 		],
 	],
-
+	themes: ['@docusaurus/theme-search-algolia'],
 	themeConfig:
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
@@ -78,6 +78,44 @@ const config = {
 				theme: prismThemes.github,
 				darkTheme: prismThemes.dracula,
 			},
+			algolia: {
+	      // The application ID provided by Algolia
+	      appId: 'M20F8W4K2K',
+
+	      // Public API key: it is safe to commit it
+	      apiKey: 'cd211d1ca55fcbcd002ef2ae5188b900',
+
+	      indexName: 'wheresrhys-co',
+
+	      // Optional: see doc section below
+	      contextualSearch: false,
+
+	      // Optional: Specify domains where the navigation should occur through
+	      // window.location instead on history.push. Useful when our Algolia config
+	      // crawls multiple documentation sites and we want to navigate with
+	      // window.location.href to them.
+	      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+	      // Optional: Replace parts of the item URLs from Algolia. Useful when
+	      // using the same search index for multiple deployments using a different
+	      // baseUrl. You can use regexp or string in the `from` param.
+	      // For example: localhost:3000 vs myCompany.com/docs
+	      // replaceSearchResultPathname: {
+	      //   from: '/docs/', // or as RegExp: /\/docs\//
+	      //   to: '/',
+	      // },
+
+	      // Optional: Algolia search parameters
+	      searchParameters: {},
+
+	      // Optional: path for search page that enabled by default (`false` to disable it)
+	      searchPagePath: 'search',
+
+	      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+	      insights: false,
+
+	      //... other Algolia params
+	    }
 		}),
 };
 
